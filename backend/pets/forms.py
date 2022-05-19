05a9c5af8e40f3aae38ba6347121d3e15bb43662
MAREK <marekidziak1@gmail.com>
2022-05-19 09:38:40 +0300

    create pet and petAvatar createUpdateForm

--- backend/pets/forms.py
+++ backend/pets/forms.py
@@ -1,12 +1,17 @@
 from .models import Pet
 from django import forms
-from django.forms.widgets import PasswordInput, TextInput, EmailInput
+from django.forms.widgets import PasswordInput, TextInput, EmailInput, NumberInput, DateInput, FileInput
 
-# class PetCreateUpdateForm(forms.ModelForm):
-#     name = forms.CharField(max_length=30, widget=TextInput(attrs={'class': 'g__form-input input','placeholder': 'dog name','type':"text"}))
-#     race = forms.CharField(max_length=30, widget=TextInput(attrs={'class': 'g__form-input input','placeholder': 'dog race','type':"text"}))
-#     year_birth = 
-#     avatar = 
-#     class Meta:
-#         model=Pet
-#         fields = ['name','race','year_birth', 'avatar']
+class PetCreateUpdateForm(forms.ModelForm):
+    name = forms.CharField(max_length=30, widget=TextInput(attrs={'class': 'g__form-input input','placeholder': 'dog name','type':"text"}))
+    race = forms.CharField(max_length=30, widget=TextInput(attrs={'class': 'g__form-input input','placeholder': 'dog race','type':"text"}))
+    year_birth = forms.IntegerField(widget=NumberInput(attrs={'class': 'g__form-input input','placeholder': 'year of birth','type':"number"}))
+    class Meta:
+        model=Pet
+        fields = ['name','race','year_birth']
+        
+class PetAvatarCreateUpdateForm(forms.ModelForm):
+    #avatar = forms.ImageField(widget=NumberInput(attrs={'class': 'g__form-input input','placeholder': 'dog race','type':"number"}))
+    class Meta:
+        model=Pet
+        fields = ['avatar']
