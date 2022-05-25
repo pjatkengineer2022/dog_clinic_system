350426bf404b8c448bfbf0e766e758c0dd37b6a7
unknown <dominikspiewak97@gmail.com>
2022-05-25 18:20:30 +0200

    feat: add page add visits days and hours

--- js/global_main.js
+++ js/global_main.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
     topNavigation();
+
+    // $('.js-add-visit-hours-input').datepicker({
+    //     multidate: true,
+    //     format: 'dd-mm-yyyy'
+    // });
+
+    $('.datepicker').datepicker({
+        format: 'mm/dd/yyyy',
+        startDate: '-1d',
+        multidate: true,
+    });
+      
 });
   
