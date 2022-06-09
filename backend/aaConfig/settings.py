6c90a5436eb01182890979ef5b0f50d5fd094603
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-09 19:12:44 +0200

    settings.py edit

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'backend/static'
+STATIC_URL = 'backend/aaStatic'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'aaStatic'), ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
