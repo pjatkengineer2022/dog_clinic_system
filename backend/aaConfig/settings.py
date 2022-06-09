6d958efaf1fc946ec9125a5062259fa8a5d067ac
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-09 16:14:29 +0200

    settings.py edit

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'backend/staticfiles/')
-STATIC_URL = 'backend/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'backend/aaStatic/'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend.staticfiles')
+STATIC_URL = 'backend.static'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'backend.aaStatic'), ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -146,5 +146,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'engineer2022db@gmail.com'
 EMAIL_HOST_PASSWORD =  'nqqlcoiyoisejfir' #'ngcjsuuqibifcqzd'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'backend/aaMedia')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend.aaMedia')
 MEDIA_URL = '/media/'
