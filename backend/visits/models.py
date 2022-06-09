de6a7e82ccaec8b586ced1d603aa867be9fd3d5d
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-09 13:40:34 +0200

    edit model.py in visits

--- backend/visits/models.py
+++ backend/visits/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from pets.models import Pet, Treatment
+from backend.pets.models import Pet, Treatment
 from doctors.models import Doctor
 # Create your models here.
 
