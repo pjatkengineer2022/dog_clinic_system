ad51fefd2ae875855596b51aa3297ea4c64685fd
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-09 12:35:16 +0200

    edit apps.py

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend.users'
+    name = 'backusers'
     def ready(self):
         import users.signals
