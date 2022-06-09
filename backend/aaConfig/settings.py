3f6a692f2f0b8c57fd19a1a8534179fe98634233
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-09 22:12:22 +0200

    settings edit

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
 ]
 
 
-WSGI_APPLICATION = 'aaConfig.wsgi.application'
+WSGI_APPLICATION = 'backend.aaConfig.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
