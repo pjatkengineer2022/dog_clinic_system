bbda24586e6aa1b7d3aced9a707083de286c1aec
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-09 21:44:30 +0200

    static edit

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'backend/aaStatic'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'aaStatic/'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'backend/aaStatic/'),]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #models PHONE NUMBER EDIT
 PHONENUMBER_DEFAULT_REGION = "PL"
