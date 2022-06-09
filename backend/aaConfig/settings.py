40c61d83dd9fdacbba436fb293d0545811f2b49d
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-09 22:49:07 +0200

    settings edit

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'aaConfig')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'aaConfig')
+STATIC_ROOT = os.path.join(BASE_DIR, 'aaStatic')
 STATIC_URL = 'static'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'aaStatic'),]
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
