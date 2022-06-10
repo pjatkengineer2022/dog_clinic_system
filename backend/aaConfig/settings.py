baeb50790286c4833ce63cefb99cb58577216a5d
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-10 02:01:20 +0200

    staticfiles_storage

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'aaStatic'),]
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #models PHONE NUMBER EDIT
