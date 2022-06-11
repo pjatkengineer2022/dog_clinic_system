fd3202326be95cb1fcee74d2e001164237e12258
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-12 00:40:06 +0200

    add aws s3 region

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -165,7 +165,7 @@ AWS_S3_FILE_OVERWRITE = TRUE
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+AWS_S3_REGION_NAME = 'eu-west-2'
 
 
 
