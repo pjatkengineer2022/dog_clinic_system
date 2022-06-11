874e56581894fb58c9047f66e4af1fe2d52d323c
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-11 13:57:04 +0200

    databases change

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -89,8 +89,8 @@ WSGI_APPLICATION = 'aaConfig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vetofficedb',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
