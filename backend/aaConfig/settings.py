56a91cc1ba43f3f0786b67a198d7c17c604e7b3a
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-12 00:56:49 +0200

    added adressing style

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -166,6 +166,6 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'eu-west-2'
-
+AWS_S3_ADDRESSING_STYLE = 'virtual'
 
 
