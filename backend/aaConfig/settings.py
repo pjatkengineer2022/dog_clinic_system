c80784dd0f76a05d79ea53f22a9f002eb17203dc
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-13 01:28:29 +0200

    users

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend.users',
+    'users',
     'backend.pets',
     'backend.doctors',
     'backend.visits',
