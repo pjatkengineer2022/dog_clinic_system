3ed1d904ae87f6975e90b2c1b06c6cc50763fc16
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-12 21:37:26 +0200

    added databse aws

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -87,10 +87,16 @@ WSGI_APPLICATION = 'aaConfig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vetofficedb',
+        'USER': 'postgres',
+        'PASSWORD':'admin123',
+        'HOST' : 'database-1.cnfr0aw0m4lg.eu-west-2.rds.amazonaws.com',
+        'PORT' : '5432'
+
     }
 }
+
 
 
 # Password validation
