491b5fb5aa02c5d33c7f10ab5856dd456030b242
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-12 13:32:04 +0200

    from none to public

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -162,12 +162,12 @@ AWS_ACCESS_KEY_ID = 'AKIATWY2MSKRLLTIJ344'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'dog-clinic-system-bucket'
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'eu-west-2'
 AWS_S3_HOST = 's3.eu-west-2.amazonaws.com'
 AWS_S3_ADDRESSING_STYLE = 'virtual'
 AWS_S3_SIGNATURE_VERSION ='s3v4'
-AWS_DEFAULT_ACL = 'public-read'
+
 
