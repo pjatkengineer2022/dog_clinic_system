9b1ad7765fb4692e5d4e50451150c53f9330d880
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-12 22:51:40 +0200

    database settings added

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -87,15 +87,15 @@ WSGI_APPLICATION = 'aaConfig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vetoffice',
-        'USER': 'postgres',
-        'PASSWORD':'admin123',
-        'HOST' : 'database-1.cnfr0aw0m4lg.eu-west-2.rds.amazonaws.com',
-        'PORT' : '5432'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 
 
