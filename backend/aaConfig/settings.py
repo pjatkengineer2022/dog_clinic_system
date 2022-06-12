b57df72be57ebeab5dfd519b6c696d906596397a
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-12 23:36:42 +0200

    aws of

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -156,11 +156,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'aaMedia')
 MEDIA_URL = '/media/'
 
 
-AWS_ACCESS_KEY_ID = 'AKIATWY2MSKRLLTIJ344'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'dog-clinic-system-bucket'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_REGION_NAME = 'eu-west-2'
+#AWS_ACCESS_KEY_ID = 'AKIATWY2MSKRLLTIJ344'
+#AWS_SECRET_ACCESS_KEY = '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'
+#AWS_STORAGE_BUCKET_NAME = 'dog-clinic-system-bucket'
+#AWS_S3_FILE_OVERWRITE = False
+#AWS_DEFAULT_ACL = None
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#AWS_S3_REGION_NAME = 'eu-west-2'
