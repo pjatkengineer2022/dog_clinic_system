2f79c4ccb4cf212cffd15480248be4fabd73cb67
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-12 21:49:27 +0200

    aws edit

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -163,3 +163,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = 'eu-west-2'
