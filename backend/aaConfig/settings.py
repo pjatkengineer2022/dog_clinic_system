096ab71f834132561d0d83a46d39c20b110e6d8e
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-12 21:47:40 +0200

    added aws

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -154,3 +154,12 @@ EMAIL_HOST_PASSWORD =  'nqqlcoiyoisejfir' #'ngcjsuuqibifcqzd'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'aaMedia')
 MEDIA_URL = '/media/'
+
+
+AWS_ACCESS_KEY_ID = 'AKIATWY2MSKRLLTIJ344'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'dog-clinic-system-bucket'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
