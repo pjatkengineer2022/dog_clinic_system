b3c772f860a516727b36d929c0521dbb492b1e8d
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-13 01:02:06 +0200

    change

--- backend/aaConfig/settings.py
+++ backend/aaConfig/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend.users',
+    'backend.usersasd',
     'backend.pets',
     'backend.doctors',
     'backend.visits',
