6ea75c6e75fba53b5d4e49f22220513b9975a39c
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-14 22:18:24 +0200

    apps view change

--- users/apps.py
+++ users/apps.py
@@ -4,6 +4,5 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-    default = False
     def ready(self):
         import users.signals
