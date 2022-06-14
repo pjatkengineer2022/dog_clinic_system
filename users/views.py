4b51b94afbab53d3074467561543ce203329b579
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-14 20:56:20 +0200

    getorcreate

--- users/views.py
+++ users/views.py
@@ -10,7 +10,6 @@ def registerUser(request):
     if not request.user.is_authenticated:
         userForm = UserRegisterForm()
         profileRegisterForm = ProfileRegisterForm()
-        Profile.objects.get_or_create(user=request.user)
         if request.method == 'POST':
             userForm = UserRegisterForm(request.POST)
             profileRegisterForm = ProfileRegisterForm(request.POST) #needed for checking typed data in next line (if profileRegisterForm.is_valid()) 
@@ -75,6 +74,7 @@ def logoutUser(request):
 @login_required
 def updateProfile(request):
     userUpdateForm = UserUpdateForm(instance=request.user)
+    Profile.objects.get_or_create(user=request.user)
     if request.method == 'POST':
         userUpdateForm = UserUpdateForm(request.POST, instance=request.user)
         profileUpdateForm = ProfileUpdateForm(request.POST, instance=request.user.profile) #needed for checking typed data in next line (if profileRegisterForm.is_valid()) 
