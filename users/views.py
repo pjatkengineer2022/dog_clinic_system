2bdcc8804f1efd2c402daa050a88cab36a56fb95
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-15 21:19:00 +0200

    76 added

--- users/views.py
+++ users/views.py
@@ -73,7 +73,7 @@ def logoutUser(request):
 @login_required
 def updateProfile(request):
     userUpdateForm = UserUpdateForm(instance=request.user)
-    global profileUpdateForm
+    profileUpdateForm = UserUpdateForm(request.POST, instance=request.user)
     if request.method == 'POST':
         userUpdateForm = UserUpdateForm(request.POST, instance=request.user)
         profileUpdateForm = ProfileUpdateForm(request.POST, instance=request.user.profile) #needed for checking typed data in next line (if profileRegisterForm.is_valid()) 
