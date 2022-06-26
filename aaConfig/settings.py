05863dac073d51f88bf0ee68574949cf960a6d56
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-27 01:37:04 +0200

    settings change

--- aaConfig/settings.py
+++ aaConfig/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'captcha',
     'phonenumber_field',
     'rest_framework',
-    
+    'storages',
     
 ]
 
@@ -152,7 +152,8 @@ EMAIL_HOST_USER = 'engineer2022db@gmail.com'
 EMAIL_HOST_PASSWORD =  'nqqlcoiyoisejfir' #'ngcjsuuqibifcqzd'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'aaMedia')
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
+MEDIA_URL = 'dog-clinic-system-bucket.s3.amazonaws.com'
 
 AWS_ACCESS_KEY_ID = 'AKIATWY2MSKRLLTIJ344'
 AWS_SECRET_ACCESS_KEY = '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'
