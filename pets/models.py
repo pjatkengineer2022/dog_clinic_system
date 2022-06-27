e18fe64b62502018866751ade0310de9f0499dfb
s15418 <75576747+s15418@users.noreply.github.com>
2022-06-27 02:00:14 +0200

    models pets change

--- pets/models.py
+++ pets/models.py
@@ -20,9 +20,9 @@ class Pet(models.Model):
         return self.name
     def save(self, *args, **kwargs):
         super().save( *args, **kwargs)			
-        img = Image.open(self.avatar.name)		 
+        #img = Image.open(self.avatar.name)		 
         #if img.height > 300 or img.width >300:	
-           # output_size= (300,300)
+            #output_size= (300,300)
             #img.thumbnail(output_size) 			
             #img.save(self.avatar.name)	
 
